d0b078d7a3a51f45bd158d3c0657634912cb3cdc
gwangho-yi <dlrhkdgh21@gmail.com>
2025-06-16 23:32:35 +0900

    import 문

--- src/main/kotlin/hello/imports/util.kt
+++
@@ -1,4 +0,0 @@
-package hello.imports
-
-fun readInt() = readLine()!!.toInt()
-fun readDouble() = readLine()!!.toDouble()
--- src/main/kotlin/hello/imports2/util.kt
+++
@@ -1,3 +0,0 @@
-package hello.imports2
-
-fun readInt() = readLine()!!.toInt()
