fc4b6f2222b968aea1b4241a694d66fc180af347
gwangho-yi <dlrhkdgh21@gmail.com>
2025-06-23 20:26:19 +0900

    클래스, 주생성자

--- src/main/kotlin/hello/object/main.kt
+++ src/main/kotlin/hello/object/main.kt
@@ -10,4 +10,8 @@ fun main() {
     val person2 = Person.person("jinseong", "hwang", 20)
 
     println(person2.fullName)
+
+
+    val person3 = Person2("jinseong", "hwang")
+    println(person3.fullName)
 }
