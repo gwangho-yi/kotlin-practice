25a80756dec64995a2d2d480beab79a497cdde3a
gwangho-yi <dlrhkdgh21@gmail.com>
2025-06-27 22:24:12 +0900

    접근제어자 설명 변경

--- src/main/kotlin/hello/클래스/접근제어자/Person.kt
+++ src/main/kotlin/hello/클래스/접근제어자/Person.kt
@@ -1,16 +1,13 @@
 package hello.클래스.접근제어자
 
 /**
- * <h3>클래스</h3>
- * 코틀린은 기본적으로 가시성 public <=> 자바는 protected
+ * ### 접근제어자
+ * 기본 가시성 public <=> 자바는 protected
+ *
  * 자바의 protected = internal + protected
- * internal : 패키지/모듈 레벨 접근
- * protected : 상속 레벨 접근
- *
- *
- *
- * * */
-// 코틀린은 기본적으로 public
+ * - internal : 패키지/모듈 레벨 접근
+ * - protected : 상속 레벨 접근
+ */
 internal class Person {
     var firstName: String = ""
     var familyName: String = ""
